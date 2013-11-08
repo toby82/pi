f0879d9cb96da4e5d4d9dd1308543a2ca9293727
toby82 <44012573@qq.com>
2013-11-09 00:07:50 +0800

    all update

--- loadavg.py
+++ loadavg.py
@@ -4,13 +4,13 @@ import os
 from time import sleep
 import commands as cmd
 sys.path.append('/root/pi/pi')
-import led02
+import get_load_avg
 snmpcmd = 'snmpwalk -v 2c -c public 192.168.1.6 .1.3.6.1.4.1.2021.10.1.3'
 while True:
-	v1 = led02.get_load_avg(snmpcmd)
-	v11 = v1.get_load_avg1()
-	print v11
-	if float(v11) > float(6):
+	ins_get = get_load_avg.get_load_avg(snmpcmd)
+	ins_getavg = v1.get_load_avg1()
+	print ins_getavg
+	if float(ins_getavg) > float(6):
 		sleep(10)
 		os.system('killall yes')
 		break
